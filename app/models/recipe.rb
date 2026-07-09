b10670b60922e6f1e3628f21aa0e82be82a96fd1
Arslan Asad <arslan.asad0101@gmail.com>
2026-07-09 12:47:40 +0500

    add recipes at realtime

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,11 +5,19 @@ class Recipe < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :recipe_likes, dependent: :destroy
 
+  after_create_commit :broadcast_prepend_to_recipes
+
   validates :title, :description, :category, presence: true
 
   def image_url
     return ActionController::Base.helpers.asset_path("default_recipe.svg") if image.blank? || image == "default_recipe.svg"
 
     image.start_with?("/") ? image : ActionController::Base.helpers.asset_path(image)
+  end
+
+  private
+
+  def broadcast_prepend_to_recipes
+    broadcast_prepend_to :recipes, target: :recipes, partial: "welcome/recipe_card", locals: { recipe: self }
   end
 end
